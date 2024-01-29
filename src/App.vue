932babc570ca20ab5f3eced7f849431b7c3c3cd0
Nana Jusolihun <nanajusolihun@gmail.com>
2024-01-29 07:18:48 +0700

    feature: create product catalog

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import PorductCatalog from "./components/ProductCatalog.vue";
+
+export default {
+  name: "App",
+  components: {
+    PorductCatalog,
+  },
+};
+</script>
+
+<template>
+  <PorductCatalog />
+
+
+</template>
